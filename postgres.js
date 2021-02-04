8c690965e51751ef0a2786343926ed58e9f2bcad
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-02-03 18:18:12 -0700

    :(

--- postgres.js
+++ postgres.js
@@ -1,8 +1,8 @@
 const Client = require('pg').Client
 
 const client = new Client({
-    connectionString: 'postgres://pthgysajhzqkog:f07ce1bba7191c2037b0dcd45860123b6c64aa33132666e7589e78ecd0b2d05a@ec2-18-204-101-137.compute-1.amazonaws.com:5432/dapbta4beorpd4'
- || 'postgresql://localhost:5432/contacts_development',
+    connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/contacts_development',
 })
+console.log(process.env.DATABASE_URL);
 
 module.exports = client;
