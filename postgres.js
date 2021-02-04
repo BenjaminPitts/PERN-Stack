a80eb503599067857be710fd03f4b944ccc9bf4e
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-02-03 18:14:26 -0700

    this work?

--- postgres.js
+++ postgres.js
@@ -1,7 +1,8 @@
 const Client = require('pg').Client
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/contacts_development',
+    connectionString: 'postgres://pthgysajhzqkog:f07ce1bba7191c2037b0dcd45860123b6c64aa33132666e7589e78ecd0b2d05a@ec2-18-204-101-137.compute-1.amazonaws.com:5432/dapbta4beorpd4'
+ || 'postgresql://localhost:5432/contacts_development',
 })
 
 module.exports = client;
